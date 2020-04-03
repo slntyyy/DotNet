e9141faf589f21b578e9911bc105d442794082a8
slnt-github <labiwei@163.com>
2020-04-03 19:56:42 +0800

    git command

--- README.md
+++ README.md
@@ -14,3 +14,12 @@ dotnet add referennce ../{test}.csproj
 dotnet add package Newtonsoft.json
 dotnet new classlib
 ```
+
+### git method 
+```
+git remote show origin
+git remote -v
+git push origin master
+git branch -r
+git log --oneline --decorate --graph 
+```
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Newtonsoft.Json;
 
 namespace TestConsole
 {
@@ -9,10 +8,7 @@ namespace TestConsole
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello dotnetcore!");
-            AliTrade.Lingla ll = new AliTrade.Lingla();
             
-            var data = ll.GetTbkOrderDetails(Convert.ToDateTime("2020-04-02"), 1, "", 1, 100);
-            Console.WriteLine(JsonConvert.SerializeObject(data));
             Console.ReadKey();
         }
     }
